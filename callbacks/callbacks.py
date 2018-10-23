d8be7126b059fb86fda02b895a766ff5bf30511a
ybabakhin <y.babakhin@gmail.com>
2018-10-24 01:38:45 +0300

    more refactoring

--- callbacks/callbacks.py
+++ callbacks/callbacks.py
@@ -1,56 +1,20 @@
-from keras.callbacks import EarlyStopping, LearningRateScheduler, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
-from callbacks.clr_callback import CyclicLR
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 from callbacks.snapshot import SnapshotCallbackBuilder
 from params import args
 import os
 
 
 def get_callback(callback, **kwargs):
-    if callback == 'early_stopping':
-        es_callback = EarlyStopping(monitor="val_lb_metric", patience=args.early_stop_patience, mode='max')
-        callbacks = [es_callback]
-
-    elif callback == 'scheduler':
-
-        def step_decay_schedule(initial_lr=1e-3):
-            '''
-            Wrapper function to create a LearningRateScheduler with step decay schedule.
-            '''
-
-            def schedule(epoch):
-                if epoch <= 15:
-                    return initial_lr
-                elif epoch <= 15 + 40:
-                    return initial_lr / 2
-                elif epoch <= 15 + 40 + 60:
-                    return initial_lr / 10
-                else:
-                    return initial_lr / 20
-
-            return LearningRateScheduler(schedule)
-
-        lr_sched = step_decay_schedule(initial_lr=1e-4)
-
-        callbacks = [lr_sched]
-
-    elif callback == 'reduce_lr':
+    if callback == 'reduce_lr':
         es_callback = EarlyStopping(monitor="val_lb_metric", patience=args.early_stop_patience, mode='max')
         reduce_lr = ReduceLROnPlateau(monitor='val_lb_metric', factor=args.reduce_lr_factor,
                                       patience=args.reduce_lr_patience,
                                       min_lr=args.reduce_lr_min, verbose=1, mode='max')
 
         callbacks = [es_callback, reduce_lr]
-
-    elif callback == 'cyclic_lr':
-        es_callback = EarlyStopping(monitor="val_loss", patience=args.early_stop_patience, min_delta=0.0005)
-        clr_triangular = CyclicLR(mode='triangular2', base_lr=args.learning_rate / 16, max_lr=args.learning_rate / 4,
-                                  step_size=2000)
-        callbacks = [es_callback, clr_triangular]
-
     elif callback == 'snapshot':
         snapshot = SnapshotCallbackBuilder(kwargs['weights_path'], args.epochs, args.n_snapshots, args.learning_rate)
         callbacks = snapshot.get_callbacks(model_prefix='snapshot', fold=kwargs['fold'])
-
     else:
         ValueError("Unknown callback")
 
--- datasets/generators.py
+++ datasets/generators.py
@@ -2,47 +2,24 @@ import cv2
 import numpy as np
 from params import args
 import os
-from albumentations import RandomCrop, CenterCrop, PadIfNeeded
+from albumentations import PadIfNeeded
 
 
 class SegmentationDataGenerator:
-    def __init__(self, input_shape=(128,128), batch_size=32, preprocess=None, augs=None):
+    def __init__(self, input_shape=(128, 128), batch_size=32, preprocess=None, augs=None):
         self.input_shape = input_shape
         self.batch_size = batch_size
         self.preprocess = preprocess
         self.augs = augs
-    
-    @staticmethod 
-    def get_down_masks(y1, new_img_size=128):
-        y2 = [resize(m, (new_img_size//2, new_img_size//2), preserve_range=True, order=3) for m in (y1) ]
-        y3 = [resize(m, (new_img_size//4, new_img_size//4), preserve_range=True, order=3) for m in (y1) ]
-        y4 = [resize(m, (new_img_size//8, new_img_size//8), preserve_range=True, order=3) for m in (y1) ]
-        y5 = [resize(m, (new_img_size//16, new_img_size//16), preserve_range=True, order=3) for m in (y1) ]
-        y2 = [(m > 0.5).astype('float32') for m in y2]
-        y3 = [(m > 0.5).astype('float32') for m in y3]
-        y4 = [(m > 0.5).astype('float32') for m in y4]
-        y5 = [(m > 0.5).astype('float32') for m in y5]
-        y2 = np.array(y2).reshape(-1, new_img_size//2, new_img_size//2, 1).astype('float32')
-        y3 = np.array(y3).reshape(-1, new_img_size//4, new_img_size//4, 1).astype('float32')
-        y4 = np.array(y4).reshape(-1, new_img_size//8, new_img_size//8, 1).astype('float32')
-        y5 = np.array(y5).reshape(-1, new_img_size//16, new_img_size//16, 1).astype('float32')
 
-        y6 = np.amax(y1, axis=(1,2), keepdims=True)
-
-        return [y6, y1, y2, y3, y4, y5, y1]    
-    
     def _read_image_train(self, id):
-        # Read with resizing
+        if os.path.isfile(os.path.join(args.images_dir, '{}.png'.format(id))):
+            img = cv2.imread(os.path.join(args.images_dir, '{}.png'.format(id)), cv2.IMREAD_COLOR)
+            mask = cv2.imread(os.path.join(args.masks_dir, '{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
 
-        
-        if os.path.isfile(os.path.join(args.images_dir,'{}.png'.format(id))):
-            img = cv2.imread(os.path.join(args.images_dir,'{}.png'.format(id)), cv2.IMREAD_COLOR)
-            mask = cv2.imread(os.path.join(args.masks_dir,'{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
-            
         else:
-            img = cv2.imread(os.path.join(args.test_folder,'{}.png'.format(id)), cv2.IMREAD_COLOR)
-            mask = cv2.imread(os.path.join(args.pseudolabels_dir,'{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
-            
+            img = cv2.imread(os.path.join(args.test_folder, '{}.png'.format(id)), cv2.IMREAD_COLOR)
+            mask = cv2.imread(os.path.join(args.pseudolabels_dir, '{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
 
         img = np.array(img, np.float32)
 
@@ -51,9 +28,9 @@ class SegmentationDataGenerator:
             augmented = self.augs(**data)
             img, mask = augmented["image"], augmented["mask"]
 
-        img = cv2.resize(img, (args.resize_size,args.resize_size))
-        mask = cv2.resize(mask, (args.resize_size,args.resize_size))
-        augmentation = PadIfNeeded(min_height=self.input_shape[0], min_width=self.input_shape[1], p=1.0, border_mode=4) 
+        img = cv2.resize(img, (args.resize_size, args.resize_size))
+        mask = cv2.resize(mask, (args.resize_size, args.resize_size))
+        augmentation = PadIfNeeded(min_height=self.input_shape[0], min_width=self.input_shape[1], p=1.0, border_mode=4)
         data = {"image": img, "mask": mask}
         augmented = augmentation(**data)
         img, mask = augmented["image"], augmented["mask"]
@@ -62,20 +39,20 @@ class SegmentationDataGenerator:
         img = self.preprocess(img)
 
         mask = np.array(mask / 255., np.float32)
-        #mask = np.where(mask < 0.5, 0, 1)
+        # mask = np.where(mask < 0.5, 0, 1)
         if len(mask.shape) < 3:
             mask = np.expand_dims(mask, axis=2)
         return (img, mask)
 
     def _read_image_valid(self, id):
-        img = cv2.imread(os.path.join(args.images_dir,'{}.png'.format(id)), cv2.IMREAD_COLOR)
-        mask = cv2.imread(os.path.join(args.masks_dir,'{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(os.path.join(args.images_dir, '{}.png'.format(id)), cv2.IMREAD_COLOR)
+        mask = cv2.imread(os.path.join(args.masks_dir, '{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
 
         img = np.array(img, np.float32)
 
-        img = cv2.resize(img, (args.resize_size,args.resize_size))
-        mask = cv2.resize(mask, (args.resize_size,args.resize_size))
-        augmentation = PadIfNeeded(min_height=self.input_shape[0], min_width=self.input_shape[1], p=1.0, border_mode=4) 
+        img = cv2.resize(img, (args.resize_size, args.resize_size))
+        mask = cv2.resize(mask, (args.resize_size, args.resize_size))
+        augmentation = PadIfNeeded(min_height=self.input_shape[0], min_width=self.input_shape[1], p=1.0, border_mode=4)
         data = {"image": img, "mask": mask}
         augmented = augmentation(**data)
         img, mask = augmented["image"], augmented["mask"]
@@ -84,12 +61,11 @@ class SegmentationDataGenerator:
         img = self.preprocess(img)
 
         mask = np.array(mask / 255., np.float32)
-        #mask = np.where(mask < 0.5, 0, 1)
+        # mask = np.where(mask < 0.5, 0, 1)
         if len(mask.shape) < 3:
             mask = np.expand_dims(mask, axis=2)
 
-        return (img, mask)
-
+        return img, mask
 
     def train_batch_generator(self, ids):
         num_images = ids.shape[0]
@@ -102,72 +78,6 @@ class SegmentationDataGenerator:
             y = np.array([x[1] for x in image_masks])
 
             yield X, y
-
-
-    def evaluation_batch_generator(self, ids):
-        num_images = ids.shape[0]
-        while True:
-            for start in range(0, num_images, self.batch_size):
-                end = min(start + self.batch_size, num_images)
-
-                image_masks = [self._read_image_valid(x) for x in ids[start:end]]
-
-                X = np.array([x[0] for x in image_masks])
-                y = np.array([x[1] for x in image_masks])
-
-                yield X, y
-
-                
-class ClassificationDataGenerator:
-    def __init__(self, input_shape=(101,101), batch_size=32, preprocess=None, augs=None):
-        self.input_shape = input_shape
-        self.batch_size = batch_size
-        self.preprocess = preprocess
-        self.augs = augs
-        
-    def _read_image_train(self, id):
-        img = cv2.imread(os.path.join(args.images_dir,'{}.png'.format(id)), cv2.IMREAD_COLOR)
-        mask = cv2.imread(os.path.join(args.masks_dir,'{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
-        
-        y = 1 if np.max(mask) == 255 else 0
-        
-        img = np.array(img, np.float32)
-
-        if self.augs:
-            data = {"image": img}
-            augmented = self.augs(**data)
-            img = augmented["image"]
-        
-        img = np.array(img, np.float32)
-        img = self.preprocess(img)
-        
-        return (img, y)
-
-
-    def _read_image_valid(self, id):
-        img = cv2.imread(os.path.join(args.images_dir,'{}.png'.format(id)), cv2.IMREAD_COLOR)
-        mask = cv2.imread(os.path.join(args.masks_dir,'{}.png'.format(id)), cv2.IMREAD_GRAYSCALE)
-        
-        y = 1 if np.max(mask) == 255 else 0
-        
-        img = np.array(img, np.float32)
-        img = self.preprocess(img)
-        
-        return (img, y)
-    
-
-    def train_batch_generator(self, ids):
-        num_images = ids.shape[0]
-        while True:
-            idx_batch = np.random.randint(low=0, high=num_images, size=self.batch_size)
-
-            image_masks = [self._read_image_train(x) for x in ids[idx_batch]]
-
-            X = np.array([x[0] for x in image_masks])
-            y = np.array([x[1] for x in image_masks])
-
-            yield X, y
-
 
     def evaluation_batch_generator(self, ids):
         num_images = ids.shape[0]
