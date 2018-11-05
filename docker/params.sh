0707e6a092373dfe6f1cf1d82f6a01a1c85fea96
ybabakhin <y.babakhin@gmail.com>
2018-11-06 00:07:25 +0300

    settings fix

--- docker/params.sh
+++ docker/params.sh
@@ -6,6 +6,6 @@ IMAGENAME="${NAME}"
 CONTNAME="--name=${NAME}"
 NET="--net=host"
 IPC="--ipc=host"
-TEST_DATA=python3 -c "import sys, json; print(json.load('../SETTINGS.json')['TEST_IMAGES_DATA'])"
+TEST_DATA=python3 -c "import sys, json; print(json.load(open('../SETTINGS.json'))['TEST_IMAGES_DATA'])"
 
 VOLUMES="-v $(pwd)/..:/workdir -v $(TEST_DATA):/test_data"
